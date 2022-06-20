dadc6dd938b42ff4f93f50d0f4fd408dcc032a7a
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-06-20 22:21:26 +0600

    loop functon and reload function added

--- js/script.js
+++ js/script.js
@@ -1,48 +1,62 @@
 //function
-//random number generator
+
+
+let min = 20;
+let max = 40;
+let num = getRandom(min, max);
+console.log(num);
+let chance = 1;
+
+//Random Number Generator
 function getRandom(min, max) {
 
     return parseInt(Math.random() * (max - min) + min);
 }
 
+//random number generator
 function startGame() {
-    let min = 20;
-    let max = 40;
-    let num = getRandom(min, max);
-    console.log(num);
     let guess = parseInt(document.getElementById('input_number').value);
-    if (guess === num) {
-        document.querySelector('#result').textContent = "You guess the correct answer!!!";
-    }
-    else if (guess > num) {
-        document.querySelector('#result').textContent = "Your guess is high!!!";
-    }
-    else {
-        document.querySelector('#result').textContent = "Your guess is low!!!";
-    }
-    //let input = document.querySelector('.guess_num_form').style.visibility = "visible";
-    //let hint = document.querySelector('#hint').innerHTML = `Guess a number between ${min} to ${max} `;
+    //check 3 chance for guess
+    if (chance <= 3) {
 
-    //document.querySelector('#submit_number').addEventListener('click',inputNumber(num))
+        if (guess === num) {
+            document.querySelector('#result').innerHTML = `<b>!!!!WHOOWHOOO!!!!<br>You guess the correct answer at ${chance}-th time</b>`;
+            document.querySelector('#result').style.color = "green";
+            document.querySelector('#input_number').setAttribute('disabled', '');
+            document.querySelector('#restart').style.visibility = "visible";
+        }
+        else if (guess > num) {
+            if (chance == 3) {
+                document.querySelector('#input_number').setAttribute('disabled', '');
+                document.querySelector('#restart').style.visibility = "visible";
+            }
+            document.querySelector('#result').innerHTML = `<b>Your guess is high!!!<br>${3 - chance} chance left</b>`;
+            document.querySelector('#result').style.color = "red";
 
+            chance++;
+        }
+        else {
+            if (chance == 3) {
+                document.querySelector('#input_number').setAttribute('disabled', '');
+                document.querySelector('#restart').style.visibility = "visible";
+            }
+            document.querySelector('#result').innerHTML = `<b>Your guess is low!!!<br>${3 - chance} chance left</b>`;
+            document.querySelector('#result').style.color = "red";
+            chance++;
+        }
+    }
 }
 
-// function inputNumber(num){
-//     let guess = parseInt(document.getElementById('input_number').value);
-//     console.log(guess);
-//     console.log(num);
-
-// }
-
-
-
-
-
-
-
-
+// form visibility set
+let form = () => {
+    document.querySelector('.guess_num_form').style.visibility = "visible";
+}
+//reload function
+function fresh() {
+    location.reload();
+}
 
 //add eventlistener
 document.querySelector('#submit_number').addEventListener('click', startGame);
-//document.querySelector('#start').addEventListener('click',startGame);
-// document.querySelector('#submit_number').addEventListener('click',inputNumber);
+document.querySelector('#start').addEventListener('click', form);
+document.querySelector('#restart').addEventListener('click', fresh);
